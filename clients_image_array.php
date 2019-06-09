b5e88191b1f37901d0aba76d44071cef329adf4b
softactor <tanveerqureshee1@gmail.com>
2019-06-09 15:00:44 +0600

    jssor slider added into front page

--- clients_image_array.php
+++ clients_image_array.php
@@ -218,7 +218,7 @@
             ],
             [
                 'title' => 'IDLC SECURITIES LIMITED',
-                'logo' => 'images/logos/idlc.png',
+                'logo' => 'images/logos/IDLC.png',
             ],
         ],
     ],
